02c5f2f8bd79c0455b302c8d994cddea4688c97c
mzkwcim <wkrak98@gmail.com>
2026-08-23 22:58:27 +0200

    fix: add required version + adjust format

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = "~> 1.15"
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5"
+      version = "~> 5.23"
+    }
+    http = {
+      source  = hashicorp / http
+      version = "~> 3.6"
     }
   }
 }
